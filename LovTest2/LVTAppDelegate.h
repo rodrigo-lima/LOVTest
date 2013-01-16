20642a2d69e17c3f6ebcce227d932240b5cc34b5
Rodrigo Lima <rodrigo.lima@oracle.com>
2013-01-16 11:45:18 -0500

    Initial Commit

---
+++ LovTest2/LVTAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  LVTAppDelegate.h
+//  LovTest2
+//
+//  Created by Rodrigo Lima on 1/16/13.
+//  Copyright (c) 2013 Rodrigo Lima. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LVTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
